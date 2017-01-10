d082688ecc6f41dbcdecf22e24abe233eed5fc41
David <shuaixin.david@gmail.com>
2017-01-10 16:14:06 -0600

    updaded noun phrase extraction

--- stanford-core-nlp-test/src/main/java/SimpleCoreNLPDemo.java
+++ stanford-core-nlp-test/src/main/java/SimpleCoreNLPDemo.java
@@ -2,6 +2,7 @@ import com.google.common.base.Functions;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import edu.stanford.nlp.ling.LabeledWord;
+import edu.stanford.nlp.simple.Document;
 import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.tregex.TregexMatcher;
@@ -48,15 +49,24 @@ public class SimpleCoreNLPDemo {
 //            System.out.println("The dependency of the second word '" + sent + "' is " + sent.incomingDependencyLabel(1));
 //        }
         SimpleCoreNLPDemo demo = new SimpleCoreNLPDemo();
-        Tree tree = demo.testParse();
-        demo.getNounPhrases(tree);
+        Document review = new Document("I can't believe all of the positive reviews. Dr. Gustin is young and inexperienced, the place is cold, and completely over priced. Went there once and will never return. Healed my back and body elsewhere.");
+        for (Sentence sent : review.sentences()) {
+            Tree parseTree = sent.parse();
+            List<String> np = demo.getNounPhrases(parseTree);
+            System.out.println(sent);
+            System.out.println(np);
+            //System.out.println(sent.)
+        }
+        /*Tree tree = demo.testParse();
+        List<String> np = demo.getNounPhrases(tree);
+        System.out.println(np);*/
     }
 
     public Tree testParse() {
         Sentence sent = new Sentence("It can contain multiple sentences.");
         Tree parseTree = sent.parse();
-        System.out.println(parseTree);
-        System.out.println(parseTree.constituents());
+        //System.out.println(parseTree);
+        //System.out.println(parseTree.constituents());
 
         return parseTree;
 
@@ -70,12 +80,12 @@ public class SimpleCoreNLPDemo {
         while (matcher.find()) {
             Tree match = matcher.getMatch();
             List<Tree> leaves = match.getLeaves();
-            System.out.println(leaves);
+            //System.out.println(leaves);
             // Some Guava magic.
             String nounPhrase = Joiner.on(' ').join(Lists.transform(leaves, Functions.toStringFunction()));
             result.add(nounPhrase);
             List<LabeledWord> labeledYield = match.labeledYield();
-            System.out.println("labeledYield: " + labeledYield);
+            //System.out.println("labeledYield: " + labeledYield);
         }
         return result;
     }
